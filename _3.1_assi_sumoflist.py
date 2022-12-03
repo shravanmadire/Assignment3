cc5b49736957ad2c815659f90674dfe3c0d0dbb4
shravanmadire <116006182+shravanmadire@users.noreply.github.com>
2022-12-03 10:40:34 +0530

    Add files via upload
    assignment3

---
+++ _3.1_assi_sumoflist.py
@@ -0,0 +1,15 @@
+list1 = [8,2,3,0,7]
+ 
+
+ 
+ 
+def sumOfList(list, size):
+    if (size == 0):
+        return 0
+    else:
+        return list[size - 1] + sumOfList(list, size - 1)
+ 
+
+total = sumOfList(list1, len(list1))
+ 
+print("Sum of all elements in given list: ", total)
